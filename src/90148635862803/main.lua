1d19534c4ce5a31d6e7a50bff46c4fa6031d899c
dawnpetal <227552754+dawnpetal@users.noreply.github.com>
2026-05-19 13:50:28 -0700

    Survive the apocalypse placeholder

---
+++ src/90148635862803/main.lua
@@ -0,0 +1,20 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "SimpleScripts",
+    Text = "Game support is coming soon!",
+    Duration = 8,
+    Icon = "rbxassetid://10723415903",
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "SimpleScripts",
+    Text = "Make sure to run the script inside the game.",
+    Duration = 8,
+    Icon = "rbxassetid://10723415903",
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "SimpleScripts",
+    Text = "Not here in the lobby!",
+    Duration = 8,
+    Icon = "rbxassetid://10723415903",
+})
